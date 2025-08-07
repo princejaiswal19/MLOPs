b6ca27f6e0947f147707108dc67617c7d3425d75
prince <masterprincescs430@gmail.com>
2025-08-07 11:45:13 +0530

    add

---
+++ dvc.py
@@ -0,0 +1,12 @@
+# data versioning DVC
+# why what and how
+
+
+# mlops  pipeline 
+
+# data ingestion -- data preparation -- feature engineering--feature selection 
+# --- model training---model evaluation
+# we will make separete file for all this and finally we will calling in a main.py file
+
+#  dvc mlflow qflow that we required
+ 
